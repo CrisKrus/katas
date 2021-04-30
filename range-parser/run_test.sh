06284a04ca8b8217f10acaa61e312a5f35f48b11
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2021-04-30 09:02:19 +0100

    test single number

--- range-parser/run_test.sh
+++ range-parser/run_test.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-find . -name '*.py' | entr python -m unittest
+# docker build -t criskrus/python-test .
+docker run --rm -v $(pwd):/app criskrus/python-test
+
+# find . -name '*.py' | entr python -m unittest
--- range-parser/test_main.py
+++ range-parser/test_main.py
@@ -1,12 +1,20 @@
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_should_fail(self):
-        self.assertEqual(True, False)
+# "2" -> [2]
+# "5-10" -> [5, 6, 7, 8, 9, 10]
+# "1-10,14, 20-25:2" -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 20, 22, 24]
 
-    def test_should_pass(self):
-        self.assertEqual(False, False)
+def range_parser(ranges):
+    return [2]
+
+
+class MyTestCase(unittest.TestCase):
+    def test_single_number(self):
+        result = range_parser("2")
+
+        expected = [2]
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
