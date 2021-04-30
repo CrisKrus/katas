3f8a5c0268c1ada91cec3931d8726783b89e034d
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2021-04-30 09:31:08 +0100

    remove commented cases

--- range-parser/test_main.py
+++ range-parser/test_main.py
@@ -1,10 +1,6 @@
 import unittest
 
 
-# "2" -> [2]
-# "5-10" -> [5, 6, 7, 8, 9, 10]
-# "1-10,14, 20-25:2" -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 20, 22, 24]
-
 def range_parser(ranges):
     numbers = ranges.split(',')
 
