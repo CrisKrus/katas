e0a35ad3424546d3efab3493c9e9ee8ca7710fb7
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2021-05-14 13:55:13 +0100

    refactor: extract expected from assertion

--- 2021-05-14-first-non-repeating-character/test_main.py
+++ 2021-05-14-first-non-repeating-character/test_main.py
@@ -7,7 +7,7 @@ def first_non_repeating_letter(letters):
     return letters
 
 # X "a" -> "a"
-#   "aa" -> ""
+# X "aa" -> ""
 #   "stress" -> "t", since the letter t only occurs once in the string, and occurs first in the string.
 #   "moonmen" -> "e"
 
@@ -16,12 +16,14 @@ class Kata(unittest.TestCase):
     def test_one_letter_should_result_same_letter(self):
         first_char = first_non_repeating_letter("a")
 
-        self.assertEqual("a", first_char)
+        expected = "a"
+        self.assertEqual(expected, first_char)
 
     def test_same_letter_repeated_multiple_times_should_result_on_empty_text(self):
         first_char = first_non_repeating_letter("aaaa")
 
-        self.assertEqual("", first_char)
+        expected = ""
+        self.assertEqual(expected, first_char)
 
 
 
