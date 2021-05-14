ac1bfc5a9d7dbac8f2dc34679a2fe409aae8c459
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2021-05-14 14:17:30 +0100

    green: multiple letter result on first non repeated letter

--- 2021-05-14-first-non-repeating-character/test_main.py
+++ 2021-05-14-first-non-repeating-character/test_main.py
@@ -1,10 +1,41 @@
 import unittest
 
 
+def fill_char_frequency(current_char_index, characters, dictionary):
+    if len(characters) is current_char_index:
+        return dictionary
+
+    character = characters[current_char_index]
+    if character not in dictionary:
+        frequency = characters.count(character)
+        dictionary[character] = frequency
+
+    return fill_char_frequency(current_char_index + 1, characters, dictionary)
+
+
 def first_non_repeating_letter(letters):
-    if len(letters) > 1:
-        return ""
-    return letters
+    # selecciono primer caracter
+    # cuento apariciones en la cadena
+    # add to dictionary
+    # selecciono siguiente caracter
+    # busco en dicctionario si existe
+    # si existe paso al siguiente
+    # si no cuento
+    # reviso el diccionario y me quedo con los caracteres que solo aparece una vez
+    # busco que caracter aparece antes
+    char_frequency = {}
+    fill_char_frequency(0, letters, char_frequency)
+
+    # elimina caracteres con mas de una aparicion
+    frequency_one_chars = []
+    for character in char_frequency:
+        if char_frequency[character] == 1:
+            frequency_one_chars.append(character)
+
+    for character in frequency_one_chars:
+        return character
+
+    return ""
 
 # X "a" -> "a"
 # X "aa" -> ""
@@ -25,6 +56,11 @@ class Kata(unittest.TestCase):
         expected = ""
         self.assertEqual(expected, first_char)
 
+    def test_multiple_letters_result_on_first_non_repeated_letter(self):
+        first_char = first_non_repeating_letter("aaaao")
+
+        expected = "o"
+        self.assertEqual(expected, first_char)
 
 
 if __name__ == '__main__':
