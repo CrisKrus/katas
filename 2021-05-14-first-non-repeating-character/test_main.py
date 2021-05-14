9b9c0d7b513b886a6d64db2911c4233964efe0ab
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2021-05-14 13:51:29 +0100

    red: same letter repeated multiple times should result on empty text

--- 2021-05-14-first-non-repeating-character/test_main.py
+++ 2021-05-14-first-non-repeating-character/test_main.py
@@ -4,17 +4,23 @@ import unittest
 def first_non_repeating_letter(letters):
     return letters
 
-# "a" -> "a"
-# "aa" -> ""
-# "stress" -> "t", since the letter t only occurs once in the string, and occurs first in the string.
-# "moonmen" -> "e"
+# X "a" -> "a"
+#   "aa" -> ""
+#   "stress" -> "t", since the letter t only occurs once in the string, and occurs first in the string.
+#   "moonmen" -> "e"
 
 
 class Kata(unittest.TestCase):
-    def test_one_letter_should_return_same_letter(self):
+    def test_one_letter_should_result_same_letter(self):
         first_char = first_non_repeating_letter("a")
 
         self.assertEqual("a", first_char)
+
+    def test_same_letter_repeated_multiple_times_should_result_on_empty_text(self):
+        first_char = first_non_repeating_letter("aaaa")
+
+        self.assertEqual("", first_char)
+
 
 
 if __name__ == '__main__':
