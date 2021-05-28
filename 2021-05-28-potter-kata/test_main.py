110bf954e8b6f3d70363144b09696dd81eeb3898
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2021-05-28 12:06:25 +0100

    extract fill list with values to function

--- 2021-05-28-potter-kata/test_main.py
+++ 2021-05-28-potter-kata/test_main.py
@@ -1,6 +1,16 @@
 import unittest
 
 
+def fill_list_with_value_until_index(list_to_fill, value, last_index):
+    res = []
+
+    for i in range(len(list_to_fill)):
+        current_value = list_to_fill[i]
+        res.append(value) if i <= last_index else res.append(current_value)
+
+    return res
+
+
 def potter_basket(book_list):
     sorted_book_list = sorted(book_list, reverse=True)
 
@@ -13,19 +23,21 @@ def potter_basket(book_list):
 
     if pack is 2:
         rest = sorted_book_list[0] - sorted_book_list[2]
-        bill = rest * 8 * 2
+        bill = rest * 8 * pack
+
         bill -= bill * 0.05
 
-        sorted_book_list[0] = sorted_book_list[1] = sorted_book_list[2]
+        sorted_book_list = fill_list_with_value_until_index(sorted_book_list, sorted_book_list[2], 1)
 
         return bill + potter_basket(sorted_book_list)
 
     if pack is 3:
         rest = sorted_book_list[0] - sorted_book_list[3]
-        bill = rest * 8 * 3
+        bill = rest * 8 * pack
         bill -= bill * 0.10
 
-        sorted_book_list[0] = sorted_book_list[1] = sorted_book_list[2] = sorted_book_list[3]
+        sorted_book_list = fill_list_with_value_until_index(sorted_book_list, sorted_book_list[3], 2)
+        print(sorted_book_list)
 
         return bill + potter_basket(sorted_book_list)
 
